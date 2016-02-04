83cb7d253fce0b10667b815ec195b28039035db6
Sam Moss <sam.moss321@gmail.com>
2016-02-04 16:03:32 -0600

    successful scrape

--- server.js
+++ server.js
@@ -2,3 +2,36 @@
  * Created by samuelmoss on 2/4/16.
  */
 var express = require('express');
+var fs = require('fs');
+var cheerio = require('cheerio');
+var request = require('request');
+
+var app = express();
+
+
+var url = process.argv[2];
+
+request(url, function(error, response, html){
+    if(!error){
+        var $ = cheerio.load(html);
+
+        var title, release, rating;
+        var json = { title : "", release : "", rating : ""};
+        $('#firstHeading').filter(function(){
+            var data =$(this);
+
+            title = data.text();
+            json.title = title;
+            console.log(json);
+        })
+    } else {
+        console.log(error);
+    }
+});
+
+
+app.listen('8081');
+
+console.log('Listening on port 8081');
+
+exports = module.exports = app;
