c490eb813c985bdb503d60819951daae0f117e1d
Sam Moss <sam.moss321@gmail.com>
2016-02-09 16:41:59 -0600

    continueApi brought back into api_call module. stable

--- api_call.js
+++ api_call.js
@@ -7,7 +7,7 @@ var centralModule = require('./server.js');
 var exports = module.exports = {};
 
 var basicUrl = 'http://en.wikipedia.org/w/api.php?';
-var queryAndFormat = 'action=query&prop=links&format=json';
+var queryAndFormat = 'action=query&prop=links&format=json&pllimit=500';
 var returnedVals = '&iwurl=&titles=';
 var list = adjacencyList.list;
 var queue = adjacencyList.queue;
@@ -31,23 +31,54 @@ function initialApi(pageName, lastDistance){
     };
     if(parsedObject.continue.plcontinue){
       var continueString = pageProcessor.continueFormatter(parsedObject.continue.plcontinue);
-      function continueCheck(){
-        new Promise (
-          function (resolve,reject){
-            resolve(continueApi.call(pageName,continueString,lastDistance));
-          }).then(
-            function(val){
-              console.log("promise is resolved");
-              if(continueApi.continueString){
-                continueString = val;
-                continueCheck();
-              }
-            })
-          }
-          continueCheck();
+      continueApi(pageName, continueString, lastDistance)
+      // function continueCheck(){
+      //   new Promise (
+      //     function (resolve,reject){
+      //       resolve(continueApi.call(pageName,continueString,lastDistance));
+      //     }).then(
+      //       function(val){
+      //         console.log("promise is resolved");
+      //         if(continueApi.continueString){
+      //           continueString = val;
+      //           continueCheck();
+      //         }
+      //       })
+      //     }
+      //     continueCheck();
         }
       }
     })
+  }
+
+var continueApi = function (pageName, continueValue,lastDistance){
+    request(basicUrl + queryAndFormat + continueValue + returnedVals + pageName,
+      function (error, response, body) {
+        console.log('continue is hit');
+      if (!error && response.statusCode == 200) {
+        var parsedObject = JSON.parse(body)
+        var pagesNumber = Object.keys(parsedObject.query.pages)[0];
+        var links = parsedObject.query.pages[pagesNumber].links
+        for(var i = 0; i < links.length; i++){
+          if(!(links[i].title.match(isMeta))){
+            list[pageName].adjacencyList.push(links[i].title);
+            queue.push(
+              {
+              name: links[i].title,
+              distance: lastDistance+=1
+            });
+          }
+        };
+        if(parsedObject.continue){
+          var continueString = pageProcessor.continueFormatter(parsedObject.continue.plcontinue);
+          contineApi(pageName, continueString, lastDistance)
+          console.log('there is a continue string and it is: ', continueString)
+          return continueString;
+        } else {
+          console.log(list[pageName].adjacencyList);
+        }
+      };
+    });
   }
 
 
--- continue_handler.js
+++ continue_handler.js
@@ -1,47 +1,19 @@
-var request = require('request');
-var adjacencyList = require('./adjacency_list.js');
-var pageProcessor = require('./string_processor.js');
-var baconChecker = require('./bacon_checker');
-
-var basicUrl = 'http://en.wikipedia.org/w/api.php?';
-var queryAndFormat = 'action=query&prop=links&format=json&pllimit=500';
-var returnedVals = '&iwurl=&titles=';
-
-var list = adjacencyList.list;
-var queue = adjacencyList.queue;
-
-var isMeta = /\:/;
-var continueString = "";
-
-var continueApi = function (pageName, continueValue,lastDistance){
-  continueString = "";
-  request(basicUrl + queryAndFormat + continueValue + returnedVals + pageName,
-    function (error, response, body) {
-      console.log('continue is hit');
-    if (!error && response.statusCode == 200) {
-      var parsedObject = JSON.parse(body)
-      var pagesNumber = Object.keys(parsedObject.query.pages)[0];
-      var links = parsedObject.query.pages[pagesNumber].links
-      for(var i = 0; i < links.length; i++){
-        if(!(links[i].title.match(isMeta))){
-          list[pageName].adjacencyList.push(links[i].title);
-          queue.push(
-            {
-            name: links[i].title,
-            distance: lastDistance+=1
-          });
-        }
-      };
-      if(parsedObject.continue){
-        continueString = pageProcessor.continueFormatter(parsedObject.continue.plcontinue);
-        console.log('there is a continue string and it is: ', continueString)
-        return continueString;
-      } else {
-        console.log(list[pageName].adjacencyList);
-      }
-    };
-  });
-}
-
-exports.call = continueApi;
-module.exports.continueString = continueString
+// var request = require('request');
+// var adjacencyList = require('./adjacency_list.js');
+// var pageProcessor = require('./string_processor.js');
+// var baconChecker = require('./bacon_checker');
+//
+// var basicUrl = 'http://en.wikipedia.org/w/api.php?';
+// var queryAndFormat = 'action=query&prop=links&format=json&pllimit=500';
+// var returnedVals = '&iwurl=&titles=';
+//
+// var list = adjacencyList.list;
+// var queue = adjacencyList.queue;
+//
+// var isMeta = /\:/;
+// var continueString = "";
+//
+//
+//
+// exports.call = continueApi;
+// module.exports.continueString = continueString
